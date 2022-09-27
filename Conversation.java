df1ca6477c89d4cacbafc4ff4af0d3b2ffad6362
lcgarner <113074918+lcgarner@users.noreply.github.com>
2022-09-26 23:41:26 -0400

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,138 @@
-class Conversation {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+/**
+* class Conversation has one method, main(), which executes
+* the code necessary to facilitate a conversation between
+* a chatbot and the user. 
+*/
+class Conversation{
+  /**
+   * Executes the conversation
+   * @param arguments
+   */
+  public static void main(String[] arguments){
+    //starts conversation
+    var user_input = new Scanner(System.in);
+    System.out.println("How many rounds?");
+
+    //stores input from user (number of rounds)
+    int numRounds = user_input.nextInt();
+
+    //creates a list in which to store the transcript
+    ArrayList <String> transcript = new ArrayList<>();
+
+    //generates a random num (for alternating between set canned/conversation starters)
+    Random randomNum = new Random(); 
+
+    //list of conversation starters
+    ArrayList <String> conversation_starters = new ArrayList<String>();
+    conversation_starters.add("What's up?");
+    conversation_starters.add("How is your day going?");
+    conversation_starters.add("What's on your mind?'");
+    conversation_starters.add("How are you?");
+    conversation_starters.add("What do you want to talk about?");
+
+    //list of canned responses
+    ArrayList <String> canned_responses = new ArrayList<String>();
+    canned_responses.add("Interesting.");
+    canned_responses.add("WOW :O");
+    canned_responses.add("Okay!!");
+    canned_responses.add("Same.");
+    canned_responses.add("I don't even know what to say...");
+    canned_responses.add("Cool dude!");
+
+    //prints line one before the loop
+    int index = randomNum.nextInt(conversation_starters.size()-1);
+    String line_one = conversation_starters.get(index); 
+    System.out.println(line_one);
+
+    //adds line one to transcript
+    transcript.add(line_one);
+
+    //creates scanner for user responses
+    Scanner response = new Scanner(System.in);
+
+    //loops through conversation according to the number of rounds selected by the user
+    for (int i=0; i<numRounds; i++){
+      String user_response = response.nextLine();
+      user_response = user_response + " ";
+      transcript.add(user_response);
+
+      //First response
+      String bot_response = "";
+
+      String [] words = user_response.split(" ");
+      //iterates user response (word by word), replaces mirror words
+      for (int j=0; j < words.length; j++){
+        if (words[j].equals("you")){
+          words[j] = "I";
+          bot_response = bot_response + words[j] +" ";
+        }
+          
+        else if (words[j].equals("I")||words[j].equals("i")){
+          words[j] = "you";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else if (words[j].equals("are")){
+          words[j] = "am";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else if (words[j].equals("was")){
+          words[j] = "were";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else if (words[j].equals("am")){
+          words[j] = "are";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else if (words[j].equals("my")){
+          words[j] = "your";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else if (words[j].equals("your")){
+          words[j] = "my";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else if (words[j].equals("me")){
+          words[j] = "you";
+          bot_response = bot_response + words[j] +" ";
+        }
+
+        else{
+          bot_response = bot_response + words[j] +" ";
+        }
+      }
+
+      //prints a canned response if user_response has no mirror words
+      if (user_response.equals(bot_response)){
+        int index2 = randomNum.nextInt(canned_responses.size()-1);
+        String canned_response = canned_responses.get(index2);
+        System.out.println(canned_response);
+        transcript.add(canned_response);
+      }
+
+      else{
+        System.out.println(bot_response);
+        transcript.add(bot_response);
+      }  
+
+  }
+
+  //ends conversation, prints transcript
+  System.out.println("Good talk! See you l8tr!! :) <3");
+  System.out.println("TRANSCRIPT");
+  System.out.println(transcript);
+
+  //closes scanners (avoids resource leak)
+  user_input.close();
+  response.close();
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    I consulted with Liberty Lehr and Nathalie Trouba on this assignment. 
 - Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
+    I attended Dakota's session on Sunday 7-9pm. They really helped me review ArrayLists and figure out the general direction of my program. 
+- What are your initial impressions of Java?
+    I have programmed in Java before this class, but in returning to Java, I have remembered just how many more syntax requirements there seem to be. It probably seems this way to me in part because Java is a compile language and will constantly remind me about errors (and possible errors) before I can even run the program. I do actually like this feature because it forces me to stay on top of the possible errors in my program before I build on top of them and they multiply. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    The basic logic of both is the same, and most of what I learned in Python is easily transferrable to Java. I find that Java has more capabilities; this tells me that when programming and starting projects I should be very careful about which language I use, as different languages have different benefits/strengths, but that the logic of a program will never change even if it is expressed in another language. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    Splitting the user's response into words really helped me. Something that did not work was including the first phrase of the conversation in the for loop. I would advise someone taking this course in the future to draw out diagrams on paper to figure out the structure of their code, especially for assignments like this where everything is just in main. This technique helped me keep everything organized. 
